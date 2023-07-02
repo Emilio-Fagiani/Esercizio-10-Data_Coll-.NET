4f03c6ef2a709afee3183fb18e03a1847a0b6aa5
emifa <emifa@LAPTOP-1TM449QB>
2023-07-02 16:28:46 +0200

    Aggiungere i file di progetto.

---
+++ Esercizio-10-Data_Coll-.NET/Program.cs
@@ -0,0 +1,43 @@
+﻿using Esercizio_10_Data_Coll_.NET;
+
+class Program
+{
+
+    static void Main()
+    {
+
+        List<Student> students = new List<Student>()
+        {
+            new Student("Pippo", 8),
+            new Student("Mario", 6),
+            new Student("Jack", 7),
+            new Student("Mimmo", 10),
+            new Student("Erik", 8),
+            new Student("Roberto", 5),
+            new Student("Gianluca", 6),
+            new Student("Ruggero", 9),
+            new Student("Dodo", 10),
+            new Student("Mimi", 9)
+        };
+        List<Student> studentsMaxGrade = MaximumGrade(students);
+        Console.WriteLine("Students with Highest Grade:");
+
+        foreach (Student student in studentsMaxGrade)
+        {
+            Console.WriteLine(student.Name);
+        }
+    }
+
+    static List<Student> MaximumGrade(List<Student> students)
+    {
+        int studsMaxGrade = students.Max(s => s.Grade);
+
+        List<Student> studentsMaxGrade = students.Where(s => s.Grade == studsMaxGrade)
+                                                            .OrderBy(s => s.Name)
+                                                            .ToList();
+        return studentsMaxGrade;
+    }
+
+
+}
+
---
+++ Esercizio-10-Data_Coll-.NET/Student.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_10_Data_Coll_.NET
+{
+    internal class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+
+        public Student(string name, int grade)
+        {
+            Name = name;
+            Grade = grade;
+        }
+
+    }
+}
